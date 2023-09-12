ebb26119990b94977051b4cdeed1e475006cd620
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-12 09:41:02 -0300

    Update exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -19,46 +19,58 @@ int main(void){
         
     switch (prato){
         case 1:
-            printf("Vegetariano: 180cal\n"); // vegetariano 180
+            printf("Vegetariano: 180"); // vegetariano 180
+            caloriasTotal = 180;
             break;
         case 2:
-            printf("Peixe: 230cal\n"); // peixe 230
+            printf("Peixe: 230"); // peixe 230
+            caloriasTotal = 230;
             break;
         case 3:
-            printf("Frango: 250cal\n"); // frango 250
+            printf("Frango"); // frango 250
+            caloriaaTotal = 250;
             break;
         case 4:
-            printf("Carne: 350cal\n"); // carne 350
+            printf("Carne"); // carne 350
+            caloriasTotal = 350;
             break;
         
     }
     switch (sobremesa){
         case 1:
-            printf("Abacaxi: 75cal\n"); // abacaxi 75
+            printf("Abacaxi"); // abacaxi 75
+            caloriasTotal += 75;
             break;
         case 2:
-            printf("Sorvete Diet: 110cal\n"); //sorvete diet 110
+            printf("Sorvete Diet"); //sorvete diet 110
+            caloriasTotal += 110;
             break;
         case 3:
-            printf("Mouse Diet: 170cal\n"); //mouse diet 170
+            printf("Mouse Diet"); //mouse diet 170
+            caloriasTotal += 170;
             break;
         case 4:
-            printf("Mouse de chocolate: 200cal\n"); //mouse de chocolate 200
+            printf("Mouse de chocolate"); //mouse de chocolate 200
+            caloriasTotal += 200;
             break;
     }
     
     switch (bebida){
         case 1:
-            printf("Chá: 30cal\n"); // chá 20
+            printf("Chá"); // chá 20
+            caloriasTotal += 20;
             break;
         case 2:
-            printf("Suco de Laranja: 70cal\n"); // suco de laranja 70
+            printf("Suco de Laranja"); // suco de laranja 70
+            caloriasTotal += 70;
              break;
         case 3:
             printf("Suco de Melão: 100cal\n"); // suco melão 100
+            caloriasTotal += 100;
             break;
         case 4:
-            printf("Refrigerante Diet: 65cal\n"); // refrigerante diet 65
+            printf("Refrigerante Diet"); // refrigerante diet 65
+            caloriasTotal += 65;
             break;
 
     }
